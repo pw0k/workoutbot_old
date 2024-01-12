d3e933007743cbc081888bf21480994f87440cc6
pw0k <pavlentor@gmail.com>
2024-01-12 16:54:41 +0300

    test with ci/cd

--- src/test/java/pw/workoutBot/WorkoutBotApplicationTests.java
+++ src/test/java/pw/workoutBot/WorkoutBotApplicationTests.java
@@ -3,11 +3,11 @@ package pw.workoutBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.main.lazy-initialization=true")
 class WorkoutBotApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
