466c8c5348c3b31144bacd21f2b560dcb7adfee3
pw0k <pavlentor@gmail.com>
2024-01-13 22:18:20 +0300

    test with ci/cd

--- src/main/java/pw/workoutBot/WorkoutBotApplication.java
+++ src/main/java/pw/workoutBot/WorkoutBotApplication.java
@@ -1,5 +1,6 @@
 package pw.workoutBot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,10 +8,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //todo linter stopbugs
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class WorkoutBotApplication {
 
 	public static void main(String[] args) {
+		logJVMProperties();
 		SpringApplication.run(WorkoutBotApplication.class, args);
+	}
+
+	private static void logJVMProperties() {
+		log.info("Max Memory: {}", Runtime.getRuntime().maxMemory());
+		log.info("Total Memory: {}", Runtime.getRuntime().totalMemory());
+		log.info("Free Memory: {}", Runtime.getRuntime().freeMemory());
+		log.info("Available Processors: {}", Runtime.getRuntime().availableProcessors());
+		log.info("Max Direct Memory Size: {}", System.getProperty("MaxDirectMemorySize"));
+		log.info("Max Metaspace Size: {}", System.getProperty("MaxMetaspaceSize"));
+		log.info("Reserved Code Cache Size: {}", System.getProperty("ReservedCodeCacheSize"));
+		log.info("Thread Stack Size: {}", System.getProperty("ThreadStackSize"));
 	}
 
 }
