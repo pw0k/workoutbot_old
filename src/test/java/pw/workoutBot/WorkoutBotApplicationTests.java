749d5520a40c9e90b6da99dbbe917d6c3221f7de
pw0k <pavlentor@gmail.com>
2024-02-06 16:25:27 +0300

    fixing  ci/cd

--- src/test/java/pw/workoutBot/WorkoutBotApplicationTests.java
+++ src/test/java/pw/workoutBot/WorkoutBotApplicationTests.java
@@ -6,7 +6,7 @@ import pw.workoutBot.model.AbstractPostgresContainer;
 
 //using AbstractPostgresContainer bcs problem with init context before @Testcontainers up
 // -> so liquibase can't connect and fall
-@SpringBootTest(classes = WorkoutBotApplication.class)
+@SpringBootTest//(classes = WorkoutBotApplication.class)
 //class WorkoutBotApplicationTests extends AbstractPostgresContainer {
 class WorkoutBotApplicationTests extends AbstractPostgresContainer{
 	@Test
